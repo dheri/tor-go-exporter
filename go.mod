4fa179a294c4e5d528ce6fbc9bd87d8661bb862a
dheri <dheri@parteek.ca>
2022-05-01 02:26:49 -0400

    more

--- go.mod
+++ go.mod
@@ -1,8 +1,12 @@
-module tor-metric-exporter
+module tor-go-exporter
 
 go 1.17
 
+require github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e
+
 require (
+	github.com/heatxsink/go-logstash v0.0.0-20140930100237-85506bb30a92 // indirect
 	github.com/reiver/go-oi v1.0.0 // indirect
-	github.com/reiver/go-telnet v0.0.0-20180421082511-9ff0b2ab096e // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
 )
--- reader.go
+++ reader.go
@@ -2,18 +2,77 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"strings"
 
 	"github.com/reiver/go-telnet"
 )
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Println("Connecting to telnet.")
 
-	var handler telnet.Handler = telnet.EchoHandler
+	conn, _ := telnet.DialTo("127.0.0.1:42457")
 
-	err := telnet.ListenAndServe(":5555", handler)
-	if nil != err {
-		//@TODO: Handle this error better.
+	conn.Write([]byte("authenticate \" \""))
+	conn.Write([]byte("\n"))
+	fmt.Println("Connected")
+
+	b := make([]byte, 6)
+	_, err := conn.Read(b)
+	fmt.Println(string(b))
+
+	if err != nil {
+		// handle error
+		fmt.Println("err.")
 		panic(err)
 	}
+
+	conn.Write([]byte("setevents  circ stream orconn addrmap status_general status_client guard"))
+	conn.Write([]byte("\n"))
+
+	fmt.Println(ReaderTelnet(conn, "19a3a590701e391bd3f4bd4ea6c7e3b6964da5c763dbd201739358312ffe760a"))
+
+}
+
+// stop function if 'expect' is encountered
+func ReaderTelnet(telnet_conn *telnet.Conn, expect string) (out string) {
+	var buffer [1]byte
+	recvData := buffer[:]
+	var n int
+	var telnet_err error
+
+	tcp_conn, tcp_err := net.Dial("tcp", "localhost:9000")
+
+	// l := logstash.New("127.0.0.1", 9000, 25)
+	// _, logstash_err := l.Connect()
+
+	if tcp_err != nil {
+		panic(tcp_err)
+	} else {
+		fmt.Println("logstash connection made sucessfully")
+	}
+
+	for {
+		n, telnet_err = telnet_conn.Read(recvData)
+		// fmt.Println("Bytes: ", n, "Data: ", recvData, string(recvData))
+		if n <= 0 || telnet_err != nil || strings.Contains(out, expect) {
+			break
+		} else {
+			out += string(recvData)
+		}
+		if recvData[0] == 10 {
+			fmt.Print(out)
+
+			_, tcp_err := tcp_conn.Write([]byte(out))
+
+			// logstash_err = l.Writeln(out)
+			if tcp_err != nil {
+				panic(tcp_err)
+			}
+
+			out = ""
+		}
+	}
+
+	return out
 }
